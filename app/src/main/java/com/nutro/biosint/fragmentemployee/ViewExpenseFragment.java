85daeb7e7602b56b470c70637878cd1f45a8e61d
murali <murali.e@sosaley.in>
2020-10-13 19:01:45 +0530

    View Expense Commit

--- app/src/main/java/com/nutro/biosint/fragmentemployee/ViewExpenseFragment.java
+++ app/src/main/java/com/nutro/biosint/fragmentemployee/ViewExpenseFragment.java
@@ -1,9 +1,12 @@
 package com.nutro.biosint.fragmentemployee;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,11 +14,19 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.nutro.biosint.R;
 import com.nutro.biosint.adapteremployee.ViewExpenseAdapter;
 import com.nutro.biosint.modelresponse.ViewExpenseResponse;
+import com.nutro.biosint.utils.PreferenceUtil;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class ViewExpenseFragment extends Fragment implements ViewExpenseAdapter.ViewExpenseClickListener {
@@ -24,30 +35,110 @@ public class ViewExpenseFragment extends Fragment implements ViewExpenseAdapter.
     ViewExpenseAdapter viewExpenseAdapter;
     List<ViewExpenseResponse> viewExpenseResponseList;
 
+    FirebaseFirestore db;
+    CollectionReference viewEmployeeExpenseCollection;
+    DocumentReference viewEmployeeExpenseDocument;
+
+    private TextView selectViewExpenseDate;
+    private String userSelectedDate;
+
+    interface DateWiseExpenseReportListener {
+
+        public void onViewExpenseReport(List<ViewExpenseResponse> viewCheckInResponseList);
+
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.employee_view_expense, container, false);
 
+        initFireBase();
+        viewEmployeeExpenseCollection = db.collection("EmployeeExpense");
+
         viewExpenseRecyclerView = (RecyclerView) view.findViewById(R.id.viewExpenseRecyclerView);
         viewExpenseRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         viewExpenseRecyclerView.setHasFixedSize(true);
+
+        selectViewExpenseDate = (TextView) view.findViewById(R.id.selectViewExpenseDate);
 
         viewExpenseResponseList = new ArrayList<>();
 
         viewExpenseAdapter = new ViewExpenseAdapter(getActivity(), viewExpenseResponseList, ViewExpenseFragment.this);
 
+        viewExpenseRecyclerView.setAdapter(viewExpenseAdapter);
 
-        getExpenseReport();
-        
+
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        selectViewExpenseDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                        int monthOfYear = month + 1;
+                        String date = dayOfMonth + "/" + monthOfYear + "/" + year;
+                        userSelectedDate = date;
+                        selectViewExpenseDate.setText("" + date);
+
+                        viewExpenseResponseList.clear();
+                        viewExpenseAdapter.notifyDataSetChanged();
+
+                        getExpenseReport(userSelectedDate, new DateWiseExpenseReportListener() {
+                            @Override
+                            public void onViewExpenseReport(List<ViewExpenseResponse> viewExpenseResponseList) {
+
+                                viewExpenseAdapter.setData(viewExpenseResponseList);
+
+                            }
+                        });
+
+
+                    }
+                }, year, month, day);
+
+                datePickerDialog.show();
+            }
+        });
+
+
         return view;
     }
 
-    private void getExpenseReport() {
+
+    private void getExpenseReport(String userSelectedDate, final DateWiseExpenseReportListener dateWiseExpenseReportListener) {
+
+        viewEmployeeExpenseCollection.whereEqualTo("managerUserId", PreferenceUtil.getManagerId(getContext()))
+                .whereEqualTo("empUserId", PreferenceUtil.getEmpUserId(getContext()))
+                .whereEqualTo("date", userSelectedDate)
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    ViewExpenseResponse viewCheckInResponse = documentSnapshot.toObject(ViewExpenseResponse.class);
+                    viewExpenseResponseList.add(viewCheckInResponse);
+                }
+
+                dateWiseExpenseReportListener.onViewExpenseReport(viewExpenseResponseList);
+
+            }
+        });
+
     }
 
     @Override
     public void onViewExpenseClick() {
 
+    }
+
+    private void initFireBase() {
+        db = FirebaseFirestore.getInstance();
     }
 }
--- app/src/main/java/com/nutro/biosint/modelresponse/ViewExpenseResponse.java
+++ app/src/main/java/com/nutro/biosint/modelresponse/ViewExpenseResponse.java
@@ -1,11 +1,17 @@
 package com.nutro.biosint.modelresponse;
 
+import com.google.firebase.firestore.PropertyName;
+
 public class ViewExpenseResponse {
 
+    @PropertyName("expensePurpuse")
     private String purposeDetail;
+    @PropertyName("expenseDetails")
     private String packsTaken;
+    @PropertyName("expenseDate")
     private String viewExpDate;
     private int viewExpStatus;
+    @PropertyName("expenseAmount")
     private String viewExpPrice;
 
     public ViewExpenseResponse(String purposeDetail, String packsTaken, String viewExpDate, int viewExpStatus, String viewExpPrice) {
@@ -16,26 +22,32 @@ public class ViewExpenseResponse {
         this.viewExpPrice = viewExpPrice;
     }
 
+    @PropertyName("expensePurpuse")
     public String getPurposeDetail() {
         return purposeDetail;
     }
 
+    @PropertyName("expensePurpuse")
     public void setPurposeDetail(String purposeDetail) {
         this.purposeDetail = purposeDetail;
     }
 
+    @PropertyName("expenseDetails")
     public String getPacksTaken() {
         return packsTaken;
     }
 
+    @PropertyName("expenseDetails")
     public void setPacksTaken(String packsTaken) {
         this.packsTaken = packsTaken;
     }
 
+    @PropertyName("expenseDate")
     public String getViewExpDate() {
         return viewExpDate;
     }
 
+    @PropertyName("expenseDate")
     public void setViewExpDate(String viewExpDate) {
         this.viewExpDate = viewExpDate;
     }
@@ -48,10 +60,12 @@ public class ViewExpenseResponse {
         this.viewExpStatus = viewExpStatus;
     }
 
+    @PropertyName("expenseAmount")
     public String getViewExpPrice() {
         return viewExpPrice;
     }
 
+    @PropertyName("expenseAmount")
     public void setViewExpPrice(String viewExpPrice) {
         this.viewExpPrice = viewExpPrice;
     }
