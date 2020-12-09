d915ba6173b6aacf5f1bbc2f5b25c074598cde89
murali <murali.e@sosaley.in>
2020-12-09 09:06:16 +0530

    Employee Current Location Added Commit

--- app/src/main/java/com/nutro/biosint/fragmentmanager/TrackEmployeeFragment.java
+++ app/src/main/java/com/nutro/biosint/fragmentmanager/TrackEmployeeFragment.java
@@ -13,17 +13,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.nutro.biosint.R;
 import com.nutro.biosint.modelresponse.ManageEmployeeResponse;
+import com.nutro.biosint.modelresponse.ViewCheckInResponse;
 import com.nutro.biosint.utils.GetMyEmpDetails;
+import com.nutro.biosint.utils.PreferenceUtil;
 
 import java.util.List;
 
 public class TrackEmployeeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-    private Spinner trackEmpSpinner;
+    FirebaseFirestore db;
+    CollectionReference viewMyEmployeeCurrentLocationCollection;
 
+    private Spinner trackEmpSpinner;
     List<ManageEmployeeResponse> employeeNameDTOList;
+
+    private GoogleMap mGoogleMap;
+    private View mapView;
+
+    Double myEmpCurLat;
+    Double myEmpCurLong;
+
+
+    interface MyEmpLocationListener {
+        public void myEmpLocation(ViewCheckInResponse viewCheckInResponse);
+    }
 
 
     public TrackEmployeeFragment(List<ManageEmployeeResponse> employeeNameDTOList) {
@@ -37,9 +59,14 @@ public class TrackEmployeeFragment extends Fragment implements AdapterView.OnIte
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.track_employee_fragment, container, false);
 
-        trackEmpSpinner = (Spinner) view.findViewById(R.id.trackEmpSpinner);
+        db = FirebaseFirestore.getInstance();
+        viewMyEmployeeCurrentLocationCollection = db.collection("EmpCurrentLocation");
 
+
+        trackEmpSpinner = (Spinner) view.findViewById(R.id.trackEmpSpinner);
         List<String> empName = GetMyEmpDetails.getEmployeeName(employeeNameDTOList);
+
+
 
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, empName);
@@ -57,13 +84,48 @@ public class TrackEmployeeFragment extends Fragment implements AdapterView.OnIte
         System.out.println("SelectedSpinnerIs " + employeeNameDTOList.get(position).getName());
         System.out.println("SelectedSpinnerId " + employeeNameDTOList.get(position).getUserId());
 
-        Toast.makeText(getActivity(), "selected " + employeeNameDTOList.get(position).getName() +" "+employeeNameDTOList.get(position).getUserId(), Toast.LENGTH_LONG).show();
+        Toast.makeText(getActivity(), "selected " + employeeNameDTOList.get(position).getName() + " " + employeeNameDTOList.get(position).getUserId(), Toast.LENGTH_LONG).show();
         //Toast.makeText(getActivity(), "selected Id " + employeeNameDTOList.get(position).getUserId(), Toast.LENGTH_LONG).show();
+
+        getMyEmpLocationdetails(employeeNameDTOList.get(position).getUserId());
 
     }
 
+
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void getMyEmpLocationdetails(String userId) {
+
+        getMyEmpLocationFromFireStore(userId, new MyEmpLocationListener() {
+            @Override
+            public void myEmpLocation(ViewCheckInResponse viewCheckInResponse) {
+
+                myEmpCurLat = viewCheckInResponse.getGeoPoint().getLatitude();
+                myEmpCurLong = viewCheckInResponse.getGeoPoint().getLongitude();
+
+            }
+        });
+    }
+
+    private void getMyEmpLocationFromFireStore(String userId, final MyEmpLocationListener myEmpLocationListener) {
+
+        viewMyEmployeeCurrentLocationCollection
+                .whereEqualTo("managerUserId", PreferenceUtil.getValueString(getContext(), PreferenceUtil.MY_MANAGER_USER_ID))
+                .whereEqualTo("empUserId", userId)
+                .get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    ViewCheckInResponse viewCheckInResponse = documentSnapshot.toObject(ViewCheckInResponse.class);
+                    myEmpLocationListener.myEmpLocation(viewCheckInResponse);
+                }
+
+            }
+        });
 
     }
 }
